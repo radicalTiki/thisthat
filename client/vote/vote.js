2df1d2b4241e35bb9753e61a3e5dd64fc5262b97
Matt Gleason <tomato@Matts-MacBook-Pro.local>
2016-03-24 00:13:21 -0600

    updated voting to pull from collections, set session so user cannot vote more than once

--- client/vote/vote.js
+++ client/vote/vote.js
@@ -12,32 +12,89 @@ Template.vote.helpers({
 		var fsFile = new FS.File(images[1]);
 		//console.log(fsFile);
 		return fsFile._id;
+	},
+	uservoted: function() {
+		return Session.get('uservoted');
+	},
+	firstvote: function(images) {
+		var fsFile = new FS.File(images[0]);
+		return (Votes.findOne({_id: fsFile._id})).votefor;
+	},
+	secondvote: function(images) {
+		if (images.length > 1) {
+			var fsFile = new FS.File(images[1]);
+			return (Votes.findOne({_id: fsFile._id})).votefor;
+		} else {
+			var fsFile = new FS.File(images[0]);
+			return (Votes.findOne({_id: fsFile._id})).voteagainst;
+		}
+		return null;
 	}
 });
 
 //add votes to collection, with client ip address mapped
 Template.vote.events({
 	'click #thisButton': function(event) {
-		if (Votes.findOne({_id: $('#thisButton').val()} == null)) {
+		if (Votes.findOne({_id: $('#thisButton').val()}) == null) {
 			Votes.insert({_id: $('#thisButton').val(), votefor: 1});
+			console.log('inserted into votes: ' + $('#thisButton').val());
+			Session.setDefault('uservoted', true);
 		} else {
 			Votes.update($('#thisButton').val(), {
-				$set: { $inc: {votefor: 1}}
+				$inc: {votefor: 1}
 			});
+			console.log('updated votes: ' + $('#thisButton').val());
+			Session.setDefault('uservoted', true);
 		}
 		
 		//console.log('images in event' + $('#thisButton').val());
 		//console.log('this button clicked');
 	},
 	'click #thatButton': function(event) {
+		if (Votes.findOne({_id: $('#thatButton').val()}) == null) {
+			Votes.insert({_id: $('#thatButton').val(), votefor: 1});
+			console.log('inserted into votes: ' + $('#thatButton').val());
+			Session.setDefault('uservoted', true);
+		} else {
+			Votes.update($('#thatButton').val(), {
+				$inc: {votefor: 1}
+			});
+			console.log('updated votes: ' + $('#thatButton').val());
+			Session.setDefault('uservoted', true);
+		}
+
 		//console.log('images in event' + $('#thatButton').val());
 		//console.log('that button clicked');
 	},
 	'click #okButton': function(event) {
+		if (Votes.findOne({_id: $('#okButton').val()}) == null) {
+			Votes.insert({_id: $('#okButton').val(), votefor: 1});
+			console.log('inserted into votes: ' + $('#okButton').val());
+			Session.setDefault('uservoted', true);
+		} else {
+			Votes.update($('#okButton').val(), {
+				$inc: {votefor: 1}
+			});
+			console.log('updated votes: ' + $('#okButton').val());
+			Session.setDefault('uservoted', true);
+		}
+
 		//console.log('images in event' + $('#okButton').val());
 		//console.log('ok button clicked');
 	},
 	'click #likeButton': function(event) {
+		if (Votes.findOne({_id: $('#likeButton').val()}) == null) {
+			Votes.insert({_id: $('#likeButton').val(), voteagainst: 1});
+			console.log('inserted into votes: ' + $('#likeButton').val());
+			Session.setDefault('uservoted', true);
+		} else {
+			Votes.update($('#likeButton').val(), {
+				$inc: {voteagainst: 1}
+			});
+			console.log('updated votes: ' + $('#likeButton').val());
+			Session.setDefault('uservoted', true);
+		}
+
 		//console.log('images in event' + $('#likeButton').val());
 		//console.log('like button clicked');
 	},
